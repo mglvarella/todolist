40da0ac83b0d8b6bfda918cef67b18a091da37b5
Miguel Varella <mig-varella1@hotmail.com>
2023-10-15 18:11:36 -0300

    "first commit"

---
+++ src/test/java/br/com/miguelvarella/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miguelvarella.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
